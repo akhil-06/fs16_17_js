a41e4dec76c9991b2828838183f8e3e258534670
akhil-06 <svam1008@gmail.com>
2024-02-23 21:32:12 +0530

    dom intro done

---
+++ day8_dom/script.js
@@ -0,0 +1,36 @@
+// let h1 = document.querySelector("#head1");
+// let h2 = document.querySelectorAll(".para");
+// let myArray = Array.from(h2);
+// console.log(myArray);
+// console.log(h1);
+// console.log(h2);
+
+
+let div1 = document.querySelector("#div1");
+console.log(div1);
+div1.innerHTML = `<h1>Div1</h1>`;
+
+
+
+
+
+
+
+
+// console.log("Hello World!!!");
+
+// let h1 = document.getElementById("head1");
+// // console.log(typeof h1);
+// h1.style.color = "cyan";
+// h1.style.backgroundColor = "black"
+// h1.style.margin = "20px"
+// h1.style.padding = "50px"
+
+// let para = document.getElementsByClassName("para");
+// for(let i=0;i<para.length;i++){
+//     console.log(para[i].style.color="red");
+// }
+// console.log(para);
+
+// let para = document.getElementsByTagName("p");
+// console.log(para);
